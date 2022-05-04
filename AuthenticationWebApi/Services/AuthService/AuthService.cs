12a97a494ecce5edb91755a141b14f7d42f61ef1
Patrick <patrick@godknows.de>
2022-05-04 22:37:26 +0200

    Create the Password Hash

--- AuthenticationWebApi/Services/AuthService/AuthService.cs
+++ AuthenticationWebApi/Services/AuthService/AuthService.cs
@@ -1,11 +1,30 @@
-﻿namespace AuthenticationWebApi.Services.AuthService
+﻿using System.Security.Cryptography;
+
+namespace AuthenticationWebApi.Services.AuthService
 {
     public class AuthService : IAuthService
     {
         public async Task<User> RegisterUser(UserDto request)
         {
-            var user = new User { Username = request.Username };
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            var user = new User
+            {
+                Username = request.Username,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt
+            };
+
             return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
