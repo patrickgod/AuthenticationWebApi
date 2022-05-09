73af6c9317f8da89ddbb55b8c63cbc0c4af7ac7b
Patrick <patrick@godknows.de>
2022-05-09 22:15:05 +0200

    Add Refresh Token Data to the User Model

---
+++ AuthenticationWebApi/Migrations/20220509201319_RefreshTokenData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthenticationWebApi.Migrations
+{
+    public partial class RefreshTokenData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TokenCreated",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TokenExpires",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TokenCreated",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TokenExpires",
+                table: "Users");
+        }
+    }
+}
--- AuthenticationWebApi/Models/User.cs
+++ AuthenticationWebApi/Models/User.cs
@@ -6,5 +6,8 @@
         public string Username { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = new byte[32];
         public byte[] PasswordSalt { get; set; } = new byte[32];
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime TokenCreated { get; set; }
+        public DateTime TokenExpires { get; set; }
     }
 }
