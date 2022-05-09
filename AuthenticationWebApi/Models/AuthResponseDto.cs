21dfc6f07a14b1dad33c2933e2314c1dac4aae6b
Patrick <patrick@godknows.de>
2022-05-09 20:50:15 +0200

    Add an AuthResponseDto

---
+++ AuthenticationWebApi/Models/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticationWebApi.Models
+{
+    public class AuthResponseDto
+    {
+        public bool Success { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}
